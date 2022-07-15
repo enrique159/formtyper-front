b782e35598403bc950eafeeb107ae3d9875e993e
Enrique Marin <enrique_marin_h@hotmail.com>
2022-07-15 16:52:19 -0600

    Add PWA

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'FormTyper',
+    themeColor: '#f8f8f8',
+    msTileColor: '#FFFFFF',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'dev/sw.js',
+    }
+  }
 })
