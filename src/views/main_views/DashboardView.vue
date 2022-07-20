42bcdda4555b19fa6615f8509b52af149a5a1153
Enrique Marin <enrique_marin_h@hotmail.com>
2022-07-20 11:57:00 -0600

    Add Name to welcome dashboard title

--- src/views/main_views/DashboardView.vue
+++ src/views/main_views/DashboardView.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="dashboard animation-fade-left">
     <v-container class="px-8 px-sm-3">
-      <h1>Bienvenido</h1>
+      <h1>Bienvenido {{ user.name }}</h1>
     </v-container>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'DashboardView',
   metaInfo: { title: 'Inicio' },
+  computed: {
+    ...mapGetters({
+      user: 'getUser'
+    })
+  },
 }
 </script>
 
