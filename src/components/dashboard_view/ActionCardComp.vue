53f939c52b6b0f1fcbb12b850375330de40087c2
Enrique Marin <enrique_marin_h@hotmail.com>
2022-07-28 09:45:52 -0600

    Add ActionCards

---
+++ src/components/dashboard_view/ActionCardComp.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="action-card br-big pa-3" :class="`background-${color}`" @click="onClick()">
+    <v-icon>mdi-{{ info.icon }}</v-icon>
+    <div>
+      <h3 class="mb-0 tc-white">{{ info.title}}</h3>
+      <span class="ts-smaller tc-white">{{ info.subtitle }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ActionCardComp',
+  props: {
+    color: {
+      type: String,
+      default: 'primary'
+    },
+    info: {
+      type: Object,
+      default: () => ({})
+    },
+    action: {
+      type: Function,
+      default: () => {}
+    }
+  },
+  methods: {
+    onClick() {
+      this.action()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.action-card {
+  width: 100%;
+  height: 128px;
+  box-shadow: var(--bs-normal);
+  color: var(--color-white) !important;
+  transition: var(--transition-normal);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  cursor: pointer;
+  i { color: rgb(255, 255, 255) !important; }
+  &:hover {
+    transform: scale(0.98);
+    box-shadow: var(--bs-dark);
+  }
+}
+
+.background-primary {
+  background-color: var(--color-primary);
+}
+
+.background-blue {
+  background: linear-gradient(to right top, #008fff, #00acff, #00c4ff, #00d9ef, #12ebd9);
+}
+
+.background-orange {
+  background: linear-gradient(to right top, #ff7b3e, #ff725a, #ff6e73, #f76e89, #ea729c);
+}
+</style>
--- src/views/main_views/DashboardView.vue
+++ src/views/main_views/DashboardView.vue
@@ -3,14 +3,46 @@
     <v-container class="px-8 px-sm-3">
       <h2>Bienvenido {{ user.name }}</h2>
     </v-container>
+    <v-container>
+      <v-row>
+        <v-col cols="6" sm="4" md="3">
+          <ActionCardComp color="blue" :info="newAffiliate" :action="newAffiliate.action"/>
+        </v-col>
+        <v-col cols="6" sm="4" md="3">
+          <ActionCardComp color="orange" :info="newMember" :action="newMember.action" />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+import ActionCardComp from '@/components/dashboard_view/ActionCardComp.vue'
 import { mapGetters } from 'vuex'
 export default {
   name: 'DashboardView',
   metaInfo: { title: 'Inicio' },
+  components: { ActionCardComp },
+  data() {
+    return {
+      newAffiliate: {
+        icon: 'account-plus',
+        title: 'Nuevo Afiliado',
+        subtitle: 'Registra un nuevo afiliado de Morena',
+        action: () => {
+          this.$router.push({ name: 'affiliate-new' })
+        }
+      },
+      newMember: {
+        icon: 'account-multiple-plus',
+        title: 'Nuevo Miembro',
+        subtitle: 'Registra un nuevo miembro de la asociación',
+        action: () => {
+          this.$router.push({ name: 'member-new' })
+        }
+      }
+    }
+  },
   computed: {
     ...mapGetters({
       user: 'getUser'
